9ee89a425864fb7228d3552893bfe93c27b4af0c
arfa <arfa@example.com>
2019-12-03 15:06:39 +0700

    weshop e-commerce

---
+++ function/helper.php
@@ -0,0 +1,93 @@
+<?php
+	
+	define("BASE_URL", "http://localhost/weshop/" );
+
+	$arrayStatusPesanan[0] = "Menunggu Pembayaran";
+	$arrayStatusPesanan[1] = "Pembayaran Sedang Di Validasi ";
+	$arrayStatusPesanan[2] = "Lunas";
+	$arrayStatusPesanan[3] = "Pembayaran Di Tolak";
+
+	function rupiah($niali = 0){
+		$string = "Rp,".number_format($niali);
+		return $string;
+	}
+
+	function kategori ($kategori_id = false){
+
+		global $koneksi;
+		$string = "<div id='menu-kategori'>";
+					
+			$string .= "<ul>";
+
+							$query = mysqli_query($koneksi, "SELECT * FROM kategori WHERE status='on' ");
+
+							while ($row = mysqli_fetch_assoc($query)) {
+								$kategori = strtolower($row['kategori']);
+								if ($kategori_id == $row['kategori_id']) {
+									$string .= "<li><a href='".BASE_URL."$row[kategori_id]/$kategori.html' class='active'>$row[kategori]</a></li>";
+								} else {
+									$string .= "<li><a href='".BASE_URL."$row[kategori_id]/$kategori.html'>$row[kategori]</a></li>";
+								}
+							}
+			$string .= "</ul>";
+		$string .= "</div>";
+
+		return $string;
+	}
+
+	function admin_only($module, $level){
+		if ($level != "superadmin") {
+			$admin_page = array("kategori", "barang", "kota", "user", "banner");
+			if (in_array($module, $admin_page)) {
+				header("location:".BASE_URL);
+			}
+		}
+	}
+
+	function pagination ($query, $data_per_halaman, $pagination, $url){
+		$total_data  = mysqli_num_rows($query);
+		$total_halaman = ceil($total_data / $data_per_halaman);
+
+		$batasPosisiNomor = 6;
+		$batasJumblahHalaman = 10;
+		$mulaiPagination = 1;
+		$batasAkhirPagination = $total_halaman;
+ 
+		echo "<ul class='pagination'>";
+
+		if ($pagination > 1) {
+			$prev = $pagination - 1;
+			echo "<li><a href='".BASE_URL."$url&pagination=$prev'><< Prev</a></li>";
+		}
+
+		if ($total_halaman >= $batasJumblahHalaman) {
+			
+			if ($pagination > $batasPosisiNomor) {
+				$mulaiPagination = $pagination - ($batasPosisiNomor - 1);
+			}
+
+			$batasAkhirPagination = ($mulaiPagination - 1) + $batasJumblahHalaman;
+
+			if ($batasAkhirPagination > $total_halaman) {
+				$batasAkhirPagination = $total_halaman;
+			}
+		}
+		
+		for ($i= $mulaiPagination; $i<= $batasAkhirPagination; $i++) { 
+			if ($pagination == $i) {
+				echo "<li><a class='active' href='".BASE_URL."$url&pagination=$i'>$i</a></li>";
+			} else {
+				echo "<li><a href='".BASE_URL."$url&pagination=$i'>$i</a></li>";
+			}
+		}
+
+
+		if ($pagination < $total_halaman) {
+			$next = $pagination + 1;
+			echo "<li><a href='".BASE_URL."$url&pagination=$next'>Next >></a></li>";
+		}
+		echo "</u>";
+	}
+
+
+?>
---
+++ module/kota/list.php
@@ -0,0 +1,48 @@
+<div id="frame-tambah">
+	<a class="tombol-action" href="<?php echo BASE_URL."index.php?page=my_profile&module=kota&action=form"; ?>">+ Tambah Kota</a>
+</div>
+
+<?php
+
+	$pagination = isset($_GET["pagination"]) ? $_GET["pagination"] : 1;
+	$data_per_halaman = 5;
+	$mulai_dari = ($pagination-1) * $data_per_halaman;
+
+	$queryKota = mysqli_query($koneksi, "SELECT * FROM kota ORDER BY kota ASC LIMIT $mulai_dari, $data_per_halaman");
+	
+	if(mysqli_num_rows($queryKota) == 0){
+		echo "<h3>Saat ini belum ada nama kota yang didalam database.</h3>";
+	}
+	else{
+		echo "<table class='table-list'>";
+		
+			echo "<tr class='baris-title'>
+					<th class='kolom-nomor'>No</th>
+					<th class='kiri'>Kota</th>
+					<th class='kiri'>Tarif</th>
+					<th class='tengah'>Status</th>
+					<th class='tengah'>Action</th>
+				 </tr>";
+				 
+			$no = 1;
+			while($rowKota=mysqli_fetch_assoc($queryKota)){
+				echo "<tr>
+						<td class='kolom-nomor'>$no</td>
+						<td>$rowKota[kota]</td>
+						<td>".rupiah($rowKota['tarif'])."</td>
+						<td class='tengah'>$rowKota[status]</td>
+						<td class='tengah'>
+							<a class='tombol-action' href='".BASE_URL."index.php?page=my_profile&module=kota&action=form&kota_id=$rowKota[kota_id]"."'>Edit</a>
+							<a class='tombol-action' href='".BASE_URL."module/kota/action.php?button=Delete&kota_id=$rowKota[kota_id]'>Delete</a>
+						</td>
+					 </tr>";
+				
+				$no++;
+			}
+		
+		echo "</table>";
+
+		$queryHitungKota = mysqli_query($koneksi, "SELECT * FROM kota");
+		pagination ($queryHitungKota, $data_per_halaman, $pagination, "index.php?page=my_profile&module=kota&action=list");
+	}
+?>
